03b92bae839b2df89dbd368c6c4773388f4771da
Bruno Mota <bruno12mota@gmail.com>
2023-02-24 17:14:48 +0000

    replicate rename bug

---
+++ app/about/page-content.js
@@ -0,0 +1,5 @@
+'use client';
+
+export default function AboutContent() {
+  return <div>About</div>
+}
---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+import AboutContent from './page-content';
+
+export default function About() {
+  return <AboutContent />
+}
